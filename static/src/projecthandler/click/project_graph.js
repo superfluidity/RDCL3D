6e3cd80c83b12aaada72d738f92dd8316a1a92a2
StefanoSalsano <stefano.salsano@uniroma2.it>
2016-12-16 09:38:41 +0100

    separating gui_properties in a file

--- static/src/projecthandler/click/project_graph.js
+++ static/src/projecthandler/click/project_graph.js
@@ -111,23 +111,3 @@ function openEditor(project_id){
     window.location.href='/projects/'+project_id+'/descriptors/'+$.urlParam('type')+'/'+$.urlParam('id');
 }
 
-
-var example_gui_properties = {
-  "default": {
-    "shape": "circle",
-    "color": "#42f44e",
-    "label_color": "black",
-    "size": 15
-  },
-  "nodes": {
-    "click": {
-      "shape": "circle",
-      //"#00CC66",
-      "color": "#F27220",
-      "size": 15,
-      "name": "click"
-    }
-
-  },
-  "graphs": null
-}
