e2e0c4574fc90a16017cab9f9c98e3c114858471
StefanoSalsano <stefano.salsano@uniroma2.it>
2017-01-05 00:34:57 +0100

    tosca commit #6

--- lib/tosca/tosca_rdcl_graph.py
+++ lib/tosca/tosca_rdcl_graph.py
@@ -1,12 +1,11 @@
-# import sys
-# sys.path.append("/home/user/RDCL/heat-translator/") 
-
 import json
 import logging
 import copy
+import six
 from lib.rdcl_graph import RdclGraph
 
-# from toscaparser.tosca_template import ToscaTemplate
+from toscaparser.tosca_template import ToscaTemplate
+from translator.hot.tosca_translator import TOSCATranslator
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('ToscaRdclGraph')
@@ -32,41 +31,58 @@ class ToscaRdclGraph(RdclGraph):
             positions = json_project['positions'] if 'positions' in json_project else False
             log.debug('build graph from project json')
 
-            print json.dumps(json_project, sort_keys=True, indent=4, separators=(',', ': '))
+            # print json.dumps(json_project, sort_keys=True, indent=4, separators=(',', ': '))
 
-            # path = '/home/user/RDCL/heat-translator/translator/tests/data/network/tosca_two_servers_one_network.yaml'
-            # # tosca = ToscaTemplate(path, parsed_params, a_file)
-            # tosca = ToscaTemplate(path, {}, True)
-            # version = tosca.version
-            # if tosca.version:
-            #     print("\nversion: " + version)
+            path = '/home/user/RDCL/heat-translator/translator/tests/data/network/tosca_two_servers_one_network.yaml'
+            # tosca = ToscaTemplate(path, parsed_params, a_file)
+            tosca = ToscaTemplate(path, {}, True)
+            version = tosca.version
+            if tosca.version:
+                print("\nversion: " + version)
 
-            # if hasattr(tosca, 'description'):
-            #     description = tosca.description
-            #     if description:
-            #         print("\ndescription: " + description)
+            if hasattr(tosca, 'description'):
+                description = tosca.description
+                if description:
+                    print("\ndescription: " + description)
 
-            # if hasattr(tosca, 'inputs'):
-            #     inputs = tosca.inputs
-            #     if inputs:
-            #         print("\ninputs:")
-            #         for input in inputs:
-            #             print("\t" + input.name)
+            if hasattr(tosca, 'inputs'):
+                inputs = tosca.inputs
+                if inputs:
+                    print("\ninputs:")
+                    for input in inputs:
+                        print("\t" + input.name)
 
-            # if hasattr(tosca, 'nodetemplates'):
-            #     nodetemplates = tosca.nodetemplates
-            #     if nodetemplates:
-            #         print("\nnodetemplates:")
-            #         for node in nodetemplates:
-            #             print("\t" + node.name)
+            if hasattr(tosca, 'nodetemplates'):
+                nodetemplates = tosca.nodetemplates
+                if nodetemplates:
+                    print("\nnodetemplates:")
+                    for node in nodetemplates:
+                        print("\t" + node.name)
 
-            # if hasattr(tosca, 'outputs'):
-            #     outputs = tosca.outputs
-            #     if outputs:
-            #         print("\noutputs:")
-            #         for output in outputs:
-            #             print("\t" + output.name)
+            if hasattr(tosca, 'outputs'):
+                outputs = tosca.outputs
+                if outputs:
+                    print("\noutputs:")
+                    for output in outputs:
+                        print("\t" + output.name)
 
+            for node in tosca.graph.nodetemplates:
+                if node.name in tosca.graph.vertices:
+                    print 'node '+node.name+' is related to:'
+                    self.add_node(node.name, node.type, 'vnf', positions, graph_object)
+                    related = tosca.graph.vertex(node.name).related_nodes
+                    for related_node in related:
+                        print related_node.name + '->' + tosca.graph.vertex(node.name).related[related_node].type
+
+            # #THIS IS FOR THE TRANSLATION INTO HOT TEMPLATES
+            # translator = TOSCATranslator(tosca, {}, False,
+            #                              csar_dir=None)
+            # yaml_files = translator.output_to_yaml_files_dict('output.yaml')
+            # for name, content in six.iteritems(yaml_files):
+            #     if name != "output.yaml":
+            #         with open(name, 'w+') as f:
+            #             f.write(content)
+            # print(yaml_files['output.yaml'])
 
         except Exception as e:
             log.error('Exception in build_graph_from_project')
---
+++ static/src/projecthandler/tosca/project_graph.js
@@ -0,0 +1,113 @@
+
+//ManoGraphEditor instance
+var graph_editor = new dreamer.ToscaGraphEditor();
+var selected_vnffgId = null;
+var show_all = null;
+
+// Enable Drop Action on the Graph
+initDropOnGraph();
+
+// get Url parameters
+$.urlParam = function(name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results == null) {
+        return null;
+    } else {
+        return results[1] || 0;
+    }
+}
+
+
+$(document).ready(function() {
+    var descriptor_type = $.urlParam('type');
+    var type = descriptor_type == 'click'  ? ['click'] : ['click'];
+    var params = {
+        node: {
+            type: type,
+            group: []
+        },
+        link: {
+            group: [type],
+            view: []
+        }
+    }
+    graph_editor.addListener("filters_changed", changeFilter);
+
+
+    graph_editor.addListener("right_click_node", function(a, args) {
+        //console.log("node_selected", a, args);
+        $('#modal_edit_descriptor').modal('show');
+    });
+
+    // graph_editor initialization
+    graph_editor.init({
+        width: $('#graph_ed_container').width(),
+        height: $('#graph_ed_container').height(),
+        gui_properties: example_gui_properties,
+        descriptor_id: $.urlParam('id')
+    });
+   // graph_editor.handleFiltersParams(params);
+
+    $('#draggable-container').hide();
+    $('#vnffg_options').hide();
+});
+
+var filters = function(e, params) {
+    graph_editor.handleFiltersParams(params);
+    $('#' + e).nextAll('li').remove();
+}
+
+function generateUID() {
+    return ("0000" + (Math.random() * Math.pow(36, 4) << 0).toString(36)).slice(-4)
+}
+
+function initDropOnGraph(){
+
+var dropZone = document.getElementById('graph_ed_container');
+dropZone.ondrop = function(e) {
+    var group = graph_editor.getCurrentGroup()
+    e.preventDefault();
+    var nodetype = e.dataTransfer.getData("text/plain");
+    if (nodetype) {
+        var type_name = graph_editor.getTypeProperty()[nodetype].name;
+    }
+
+}
+
+dropZone.ondragover = function(ev) {
+    console.log("ondragover");
+    return false;
+}
+
+dropZone.ondragleave = function() {
+    console.log("ondragleave");
+    return false;
+}
+}
+
+function handleForce(el) {
+    if (el.id == "topology_play") {
+        $("#topology_pause").removeClass('active');
+        $("#topology_play").addClass('active');
+    } else {
+        $("#topology_pause").addClass('active');
+        $("#topology_play").removeClass('active');
+    }
+
+    graph_editor.handleForce((el.id == "topology_play") ? true : false);
+
+}
+
+function savePositions(el) {
+    graph_editor.savePositions();
+}
+
+function changeFilter(e, c) {
+    console.log("changeFilter");
+
+}
+
+function openEditor(project_id){
+    window.location.href='/projects/'+project_id+'/descriptors/'+$.urlParam('type')+'/'+$.urlParam('id');
+}
+
