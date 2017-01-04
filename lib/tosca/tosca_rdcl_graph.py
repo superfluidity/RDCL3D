5d7072b36309a19656acb5b4fa9bb200021eb426
StefanoSalsano <stefano.salsano@uniroma2.it>
2017-01-04 09:45:29 +0100

    tosca development compatibility fix

--- lib/tosca/tosca_rdcl_graph.py
+++ lib/tosca/tosca_rdcl_graph.py
@@ -1,12 +1,12 @@
-import sys
-sys.path.append("/home/user/RDCL/heat-translator/") 
+# import sys
+# sys.path.append("/home/user/RDCL/heat-translator/") 
 
 import json
 import logging
 import copy
 from lib.rdcl_graph import RdclGraph
 
-from toscaparser.tosca_template import ToscaTemplate
+# from toscaparser.tosca_template import ToscaTemplate
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('ToscaRdclGraph')
@@ -34,38 +34,38 @@ class ToscaRdclGraph(RdclGraph):
 
             print json.dumps(json_project, sort_keys=True, indent=4, separators=(',', ': '))
 
-            path = '/home/user/RDCL/heat-translator/translator/tests/data/network/tosca_two_servers_one_network.yaml'
-            # tosca = ToscaTemplate(path, parsed_params, a_file)
-            tosca = ToscaTemplate(path, {}, True)
-            version = tosca.version
-            if tosca.version:
-                print("\nversion: " + version)
+            # path = '/home/user/RDCL/heat-translator/translator/tests/data/network/tosca_two_servers_one_network.yaml'
+            # # tosca = ToscaTemplate(path, parsed_params, a_file)
+            # tosca = ToscaTemplate(path, {}, True)
+            # version = tosca.version
+            # if tosca.version:
+            #     print("\nversion: " + version)
 
-            if hasattr(tosca, 'description'):
-                description = tosca.description
-                if description:
-                    print("\ndescription: " + description)
+            # if hasattr(tosca, 'description'):
+            #     description = tosca.description
+            #     if description:
+            #         print("\ndescription: " + description)
 
-            if hasattr(tosca, 'inputs'):
-                inputs = tosca.inputs
-                if inputs:
-                    print("\ninputs:")
-                    for input in inputs:
-                        print("\t" + input.name)
+            # if hasattr(tosca, 'inputs'):
+            #     inputs = tosca.inputs
+            #     if inputs:
+            #         print("\ninputs:")
+            #         for input in inputs:
+            #             print("\t" + input.name)
 
-            if hasattr(tosca, 'nodetemplates'):
-                nodetemplates = tosca.nodetemplates
-                if nodetemplates:
-                    print("\nnodetemplates:")
-                    for node in nodetemplates:
-                        print("\t" + node.name)
+            # if hasattr(tosca, 'nodetemplates'):
+            #     nodetemplates = tosca.nodetemplates
+            #     if nodetemplates:
+            #         print("\nnodetemplates:")
+            #         for node in nodetemplates:
+            #             print("\t" + node.name)
 
-            if hasattr(tosca, 'outputs'):
-                outputs = tosca.outputs
-                if outputs:
-                    print("\noutputs:")
-                    for output in outputs:
-                        print("\t" + output.name)
+            # if hasattr(tosca, 'outputs'):
+            #     outputs = tosca.outputs
+            #     if outputs:
+            #         print("\noutputs:")
+            #         for output in outputs:
+            #             print("\t" + output.name)
 
 
         except Exception as e:
