f43d2041cb2c699976f8eb8d9e28e629f2bff29c
Francesco Lombardo <franclombardo@gmail.com>
2017-01-15 22:17:05 +0100

    removed redundant code

--- static/src/projecthandler/tosca/project_graph.js
+++ static/src/projecthandler/tosca/project_graph.js
@@ -58,9 +58,7 @@ var filters = function(e, params) {
     $('#' + e).nextAll('li').remove();
 }
 
-function generateUID() {
-    return ("0000" + (Math.random() * Math.pow(36, 4) << 0).toString(36)).slice(-4)
-}
+
 
 function initDropOnGraph(){
 
