2b012b3104b956e0771d2c5ba50c9b756e447ac8
Francesco Lombardo <franclombardo@gmail.com>
2017-02-24 10:47:43 +0100

    allinemento franz_ref

--- static/src/utils.js
+++ static/src/utils.js
@@ -2,87 +2,89 @@ function generateUID() {
     return ("0000" + (Math.random() * Math.pow(36, 4) << 0).toString(36)).slice(-4)
 }
 
-function cloneDescriptor(project_id, descriptor_type, descriptor_id){
-     $( "#input_choose_new_descriptor_name" ).val(descriptor_id+"_"+generateUID());
-     $('#save_new_descriptor_name').off('click').on('click', function(){
-        var new_descriptor_id = $( "#input_choose_new_descriptor_name" ).val();
+function cloneDescriptor(project_id, descriptor_type, descriptor_id) {
+    $("#input_choose_new_descriptor_name").val(descriptor_id + "_" + generateUID());
+    $('#save_new_descriptor_name').off('click').on('click', function () {
+        var new_descriptor_id = $("#input_choose_new_descriptor_name").val();
         console.log(descriptor_id);
-        window.location.href="/projects/"+project_id+"/descriptors/"+descriptor_type+"/"+descriptor_id+"/clone?newid="+new_descriptor_id ;
+        window.location.href = "/projects/" + project_id + "/descriptors/" + descriptor_type + "/" + descriptor_id + "/clone?newid=" + new_descriptor_id;
     });
     $('#modal_new_descriptor_name').modal('show');
 
 }
 
-function openDescriptorView(project_id, descriptor_type, descriptor_id){
+function openDescriptorView(project_id, descriptor_type, descriptor_id) {
     console.log("openDescriptorView", project_id, descriptor_type, descriptor_id);
-    window.location.href='/projects/' + project_id + '/descriptors/' + descriptor_type + '/' + descriptor_id;
+    window.location.href = '/projects/' + project_id + '/descriptors/' + descriptor_type + '/' + descriptor_id;
 
 }
 
-function toTitleCase(str){
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
 }
 
-function createNewDescriptor(project_id, descriptor_type){
+function createNewDescriptor(project_id, descriptor_type) {
     var type_capitalized = toTitleCase(descriptor_type)
 
-    $("#modal_chooser_new_descriptor_name").text("New "+type_capitalized+" Descriptor");
-    $("#input_choose_new_descriptor_name" ).val(descriptor_type+"_"+generateUID());
+    $("#modal_chooser_new_descriptor_name").text("New " + type_capitalized + " Descriptor");
+    $("#input_choose_new_descriptor_name").val(descriptor_type + "_" + generateUID());
 
 
-    $('#save_new_descriptor_name').off('click').on('click', function(){
-        var descriptor_id = $( "#input_choose_new_descriptor_name" ).val();
-        window.location.href='/projects/' + project_id + '/descriptors/' + descriptor_type + '/new?id='+descriptor_id ;
+    $('#save_new_descriptor_name').off('click').on('click', function () {
+        var descriptor_id = $("#input_choose_new_descriptor_name").val();
+        window.location.href = '/projects/' + project_id + '/descriptors/' + descriptor_type + '/new?id=' + descriptor_id;
     });
     $('#modal_new_descriptor_name').modal('show');
 
 }
 
-function buildPalette(args){
+function buildPalette(args) {
     var type_property = graph_editor.getTypeProperty();
 
     $("#paletteContainer").empty()
-    args.forEach(function(category){
-        var category_id = "category_"+category.category_name.replace(/\s/g, '');
-        var content_id = "palette-content-"+ category.category_name.replace(/\s/g, '');
-        $("#paletteContainer").append('<div id="'+ category_id +'" class="palette-category" ><div class="palette-header" onClick="handlePaletteCat(this);" category_id="'+ category_id +'"> '+
-                '<i class="fa fa-chevron-right fa-rotate-90"></i>'+
-                '<span>  ' + category.category_name + '</span>'+
-            '</div>'+
-            '<div id="'+ content_id +'" class="palette-content">'+
+    args.forEach(function (category) {
+        var category_id = "category_" + category.category_name.replace(/\s/g, '');
+        var content_id = "palette-content-" + category.category_name.replace(/\s/g, '');
+        $("#paletteContainer").append('<div id="' + category_id + '" class="palette-category" ><div class="palette-header" onClick="handlePaletteCat(this);" category_id="' + category_id + '"> ' +
+            '<i class="fa fa-chevron-right fa-rotate-90"></i>' +
+            '<span>  ' + category.category_name + '</span>' +
+            '</div>' +
+            '<div id="' + content_id + '" class="palette-content">' +
 
-            '</div>'+
-        '</div>');
-        category.types.forEach(function(type){
-            var palette_node_icon = (type_property[category.id].image) ? '<div class="palette-node-icon" style="background-image: url(' + (type_property[category.id].image ||  "")+ ')"></div>' :
-            '<div class="palette-node-icon" style="background-color:' + type_property[category.id].color + '"></div>'
-            var html_to_append = '<div class="palette-node ui-draggable" draggable="true" id="' + type.id + '" ondragstart="nodeDragStart(event)">'+
-                        '<div class="palette-node-label">' + type.name + '</div>'+
-                        '<div class="palette-node-icon-container">'+
-                        palette_node_icon +
-                        '</div>'+
-                    '</div>'
-            $("#"+content_id).append(html_to_append);
+            '</div>' +
+            '</div>');
+        category.types.forEach(function (type) {
+            var palette_node_icon = (type_property[category.id].image) ? '<div class="palette-node-icon" style="background-image: url(' + (type_property[category.id].image || "") + ')"></div>' :
+                '<div class="palette-node-icon" style="background-color:' + type_property[category.id].color + '"></div>'
+            var html_to_append = '<div class="palette-node ui-draggable" draggable="true" id="' + type.id + '" ondragstart="nodeDragStart(event)">' +
+                '<div class="palette-node-label">' + type.name + '</div>' +
+                '<div class="palette-node-icon-container">' +
+                palette_node_icon +
+                '</div>' +
+                '</div>'
+            $("#" + content_id).append(html_to_append);
         });
 
     });
 
 }
 
-function handlePaletteCat(item){
+function handlePaletteCat(item) {
     console.log("handlePaletteContainer")
     var category_id = $(item).attr("category_id")
-    $('#'+category_id).toggleClass("palette-open");
+    $('#' + category_id).toggleClass("palette-open");
 
 }
 
-function showAlert(msg){
+function showAlert(msg) {
     // modal_alert_text
     $('#modal_alert_text').text(msg);
     $('#modal_alert').modal('show');
 }
 
-function getUrlParameter(par_name){
+function getUrlParameter(par_name) {
     var results = new RegExp('[\?&]' + par_name + '=([^&#]*)').exec(window.location.href);
     if (results == null) {
         return null;
@@ -92,13 +94,12 @@ function getUrlParameter(par_name){
 }
 
 if (!String.format) {
-  String.format = function(format) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return format.replace(/{(\d+)}/g, function(match, number) {
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
-    });
-  };
+    String.format = function (format) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        return format.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined' ?
+                args[number] :
+                match;
+        });
+    };
 }
