4adfb219e658d4be5189f141c855aa8bd43f3d57
Francesco Lombardo <franclombardo@gmail.com>
2017-03-08 12:50:18 +0100

    fix id draganddrop

---
+++ code/static/src/projecthandler/tosca/controller.js
@@ -0,0 +1,77 @@
+if (typeof dreamer === 'undefined') {
+    var dreamer = {};
+}
+var level = {}
+
+dreamer.ToscaController = (function(global) {
+    'use strict';
+
+    var DEBUG = true;
+
+    ToscaController.prototype.constructor = ToscaController;
+
+    /**
+     * Constructor
+     */
+    function ToscaController() {
+
+
+    }
+
+
+    ToscaController.prototype.addNode = function(self, node, success, error) {
+        log('addNode');
+        new dreamer.GraphRequests().addNode(node, null, function() {
+            if (success)
+                success();
+        },error);
+    };
+
+    ToscaController.prototype.addLink = function(self, link, success, error) {
+        log('addLink');
+
+        new dreamer.GraphRequests().addLink(link, null, function() {
+            self._deselectAllNodes();
+            if (typeof old_link !== 'undefined' && old_link.length > 0 && old_link[0].index !== 'undefined') {
+                self.parent.removeLink.call(self, old_link[0].index);
+            }
+            if (success) {
+                success();
+            }
+        },error);
+    };
+
+    ToscaController.prototype.removeNode = function(self, node, success, error) {
+        log('removeNode');
+        new dreamer.GraphRequests().removeNode(node, null, function() {
+            if (success) {
+                success();
+            }
+        },error);
+    };
+
+    ToscaController.prototype.removeLink = function(self, link, success, error) {
+        log('removeLink');
+        var s = link.source;
+        var d = link.target;
+        new dreamer.GraphRequests().removeLink(link, function() {
+            if (success) {
+                success();
+            }
+        },error);
+    };
+
+    /**
+     * Log utility
+     */
+    function log(text) {
+        if (DEBUG)
+            console.log("::ToscaController::", text);
+    }
+
+    return ToscaController;
+}(this));
+
+if (typeof module === 'object') {
+    module.exports = dreamer.ToscaController;
+}
