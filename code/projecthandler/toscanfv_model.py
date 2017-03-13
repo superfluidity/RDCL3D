a5fc6e38eabf7671254e541d1195b33c8331fb65
kaarot_kalel_90 <gretopierluigi@gmail.com>
2017-03-13 14:49:29 +0100

    previous fix

--- code/projecthandler/toscanfv_model.py
+++ code/projecthandler/toscanfv_model.py
@@ -253,7 +253,7 @@ class ToscanfvProject(Project):
             vnf_template['node_types']['tosca.nodes.nfv.VNF.' + element_id] = {}
             vnf_template['node_types']['tosca.nodes.nfv.VNF.' + element_id]['derived_from'] = 'tosca.nodes.nfv.VNF'
             current_data['toscayaml'][element_id] = vnf_template
-        if 'node_template' not in current_data['toscayaml'][group_id]['topology_template'] or current_data['toscayaml'][group_id]['topology_template']['node_templates'] is None:
+        if 'node_templates' not in current_data['toscayaml'][group_id]['topology_template'] or current_data['toscayaml'][group_id]['topology_template']['node_templates'] is None:
             current_data['toscayaml'][group_id]['topology_template']['node_templates'] = {}
         current_data['toscayaml'][group_id]['topology_template']['node_templates'][element_id] = new_element
 
