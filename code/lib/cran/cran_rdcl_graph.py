8cef7dc743e82954960aff2532c631bc9ddcb03a
Francesco Lombardo <franclombardo@gmail.com>
2017-05-19 16:28:41 +0200

    working on cran graph

--- code/lib/cran/cran_rdcl_graph.py
+++ code/lib/cran/cran_rdcl_graph.py
@@ -18,6 +18,7 @@ import json
 import logging
 import copy
 from lib.rdcl_graph import RdclGraph
+from lib.util import Util
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('CranRdclGraph')
@@ -25,6 +26,7 @@ log = logging.getLogger('CranRdclGraph')
 
 class CranRdclGraph(RdclGraph):
     """Operates on the graph representation used for the GUI graph views"""
+
 
     def __init__(self):
         pass
@@ -39,11 +41,38 @@ class CranRdclGraph(RdclGraph):
             'model': model
         }
         try:
-            positions = json_project['positions'] if 'positions' in json_project else []
             log.debug('build graph from project json')
 
         except Exception as e:
             log.exception('Exception in build_graph_from_project')
             raise
 
+        return graph_object
+
+    def build_graph_from_descriptor(self, descriptor, positions, model={}):
+        """Creates a single graph for a descriptor"""
+        graph_object = {
+            'vertices': [],
+            'edges': [],
+            'graph_parameters': {},
+            'model': model
+        }
+
+        try:
+            log.debug('build graph from descriptor json')
+            functional_blocks = descriptor['Functional-blocks']
+            for f_block in functional_blocks:
+                type_fb = f_block['type'] if 'type' in f_block else 'functional_block'
+                self.add_node(f_block['name'], type_fb, False, positions, graph_object)
+                if 'rfb-list' in f_block:
+                    for rfb in f_block['rfb-list']:
+                        self.add_link(f_block['name'], rfb, 'full', None, graph_object)
+                if 'links' in f_block:
+                    for link in f_block['links']:
+                        self.add_link(f_block['name'], link, 'full', None, graph_object)
+                        self.add_link(f_block['name'], link, f_block['rfb-level'], None, graph_object)
+
+        except Exception as e:
+            log.exception('Exception in build_graph_from_descriptor')
+            raise
         return graph_object
