ffcd555676cb0fff9ad975bc03d41ca67581bf05
Francesco Lombardo <franclombardo@gmail.com>
2018-02-20 14:58:28 +0100

    k8s ansible playbook

--- code/lib/superfluidity/sf_ansible.py
+++ code/lib/superfluidity/sf_ansible.py
@@ -3,188 +3,154 @@ import yaml
 import os
 import re
 import logging
-from unsortable_ordered_dict import UnsortableOrderedDict
-from superfluidity_parser import SuperfluidityParser
+import shutil
+
+from lib.superfluidity.superfluidity_parser import SuperfluidityParser
+
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('AnsibleUtility.py')
 
 
-class AnsibleUtility(object):
+def _load_data_from_file(file_object):
+    try:
+        json_object = json.load(file_object)
+        return json_object
+    except Exception as e:
+        log.exception('Exception loadJsonFile', e)
+        raise
 
+
+class AnsibleUtility(object):
     def __init__(self):
         log.info("Constructor")
         pass
 
     def generate_playbook(self, sf_data, name, pb_path):
         try:
-            # Temp vars
-            host = "provider-master"
-            # FIXME: Should be later updated to be taken
-            # from the RDCL tool
-            # (with the proper edge/core nodes names/ips)
+            self.BASE = pb_path
+            self.roles_create = []
+            self.roles_destroy = []
+            # GENERATE A COMPLIANT APPLICATION NAME (non va bene)
+            #m = re.search('[a-z0-9]([-a-z0-9]*[a-z0-9])?', name.lower())
+            application_name = name.lower() # m.group(0)
 
-            # Regex
-            m = re.search('[a-z0-9]([-a-z0-9]*[a-z0-9])?', name.lower())
+            self.init_dir(os.path.join(self.BASE, 'group_vars'))
+            self.init_group_vars_file('all', {'application_name': str(application_name)})
 
-            application_name = m.group(0)
+            self.init_dir(os.path.join(self.BASE, 'roles'))
 
-            nc_roles = self.create_roles(sf_data, application_name)
-            roles = list(nc_roles)
+            # APP CREATE ROLE
+            self.init_dir(os.path.join(self.BASE, 'roles', application_name + '_create'))
+            self.add_role_task(application_name + '_create', 'main', [{'name': 'Create project',
+                                                                       'command': 'kubectl create namespace {{ application_name }}'}])
+            self.roles_create.append(application_name + '_create')
 
-            k8s_name = self.get_k8s_name(sf_data, nc_roles)
-            print 'nc_roles', nc_roles
-            ############################################
-            # Create folder structure
-            self.makedir_p(os.path.join(pb_path, 'group_vars'))
-            self.makedir_p(os.path.join(pb_path, 'roles'))
-            # Roles subfloders
-            for role in nc_roles:
-                self.makedir_p(os.path.join(pb_path, 'roles', role, 'tasks'))
-                if '-create' not in role and '-destroy' not in role:
-                    self.makedir_p(os.path.join(pb_path, 'roles', role, 'vars'))
-                    self.makedir_p(os.path.join(pb_path, 'roles', role, 'files'))
+            # APP DESTROY ROLE
+            self.init_dir(os.path.join(self.BASE, 'roles', application_name + '_destroy'))
+            self.add_role_task(application_name + '_destroy', 'main', [{'name': 'Destroy project',
+                                                                        'command': 'kubectl delete namespace {{ application_name }}'}])
+            self.roles_destroy.append(application_name + '_destroy')
 
-            ###########################################
-            # Various config files
+            for v in sf_data['vnfd']:
+                vnfd = sf_data['vnfd'][v]
+                for vdu in vnfd['vdu']:
+                    if 'vduNestedDesc' in vdu and vdu['vduNestedDesc'] is not None:
+                        vdu_nested_desc_id = vdu['vduNestedDesc']
+                        vdu_nested = SuperfluidityParser().get_nested_vdu_from_id(vdu_nested_desc_id, vnfd)
+                        if vdu_nested and vdu_nested['vduNestedDescriptorType'] == 'kubernetes':
+                            role_vars = {}
+                            role_name = vdu['vduId']
+                            k8s_name = str(vdu_nested['vduNestedDescriptor'])
+                            data = sf_data['k8s'][k8s_name]
+                            log.info("Generate role %s", role_name)
+                            self.add_role_template(role_name, k8s_name, data)
+                            role_vars['template_path_' + k8s_name] = '/tmp/' + k8s_name + '.j2'
+                            main_tasks = []
+                            podDependecies = self._get_properties_from_metadata(role_name, 'podDependencies', vnfd)
+                            for dep in podDependecies:
+                                for pod in podDependecies[dep]:
+                                    main_tasks.append(self.get_commands_k8s_pod_property(dep, pod))
+                            #k8s_pod_property
 
-            # Clean roles
-            roles = [str(roles[x]) for x in range(len(roles))]
+                            main_tasks.append({'name': 'Create the json file from template', 'template': {'dst': '{{template_path_' + k8s_name + '}}',
+                                                                                            'src': k8s_name + '.j2'}})
+                            main_tasks.append({'name': 'Deploy ' + k8s_name,
+                                 'command': 'kubectl create -f {{template_path_' + k8s_name + '}} --namespace={{application_name}}'
+                                 })
 
-            deploy_roles = []
-            undeploy_roles = []
-            for role in roles:
-                if '-destroy' in role:
-                    undeploy_roles.append(role)
-                else:
-                    deploy_roles.append(role)
+                            self.add_role_task(role_name, 'main', main_tasks)
 
-            # Deploy site file
-            deploy_file = [UnsortableOrderedDict(
-                [("hosts", host), ("become", True), ("become_method", "sudo"), ("vars_files", ["group_vars/all"]),
-                 ("roles", deploy_roles)])]
+                            self.add_role_var(role_name, 'main', role_vars)
+                            self.roles_create.append(str(role_name))
+            self.add_playbook_file('site_app_create', [{'hosts': 'provider-master', 'vars_files': ['group_vars/all'], 'roles': self.roles_create}])
+            self.add_playbook_file('site_app_destroy', [{'hosts': 'provider-master', 'vars_files': ['group_vars/all'], 'roles': self.roles_destroy}])
 
-            dstfile = open(pb_path + 'site_deploy.yaml', 'w')
-            UnsortableOrderedDict.dump(deploy_file, dstfile)
-            dstfile.close()
-
-            # Undeploy site file
-            undeploy_file = [UnsortableOrderedDict(
-                [("hosts", host), ("become", True), ("become_method", "sudo"), ("vars_files", ["group_vars/all"]),
-                 ("roles", undeploy_roles)])]
-
-            dstfile = open(pb_path + 'site_undeploy.yaml', 'w')
-            yaml.dump(undeploy_file, dstfile, default_flow_style=False, explicit_start=True)
-            dstfile.close()
-
-
-            ###########################################
-            # Variables
-
-            # Group vars
-            all_file = {"application_name": str(application_name)}
-
-            dstfile = open(pb_path + 'group_vars/all', 'w')
-            yaml.dump(all_file, dstfile, default_style='"', default_flow_style=False, explicit_start=True)
-            dstfile.close()
-
-            # Role vars
-            for role in nc_roles:
-                if '-create' in role or '-destroy' in role:
-                    continue
-                else:
-                    main_file = {"json_path": "/tmp/" + k8s_name[role] + ".json"}
-
-                dstfile = open(pb_path + 'roles/' + role + '/vars/main.yaml', 'w')
-                yaml.safe_dump(main_file, dstfile, default_flow_style=False, explicit_start=True)
-                dstfile.close()
-            #############################################
-            # Tasks
-
-            # Role tasks
-            for role in nc_roles:
-                if '-create' in role:
-                    main_file = [UnsortableOrderedDict(
-                        [("name", "Create " + str(name) + " project"),
-                         ("command", "kubectl create namespace {{ application_name }}")])]
-                elif '-destroy' in role:
-                    main_file = [UnsortableOrderedDict([("name", "Destroy " + str(name) + " project"),
-                                                        ("command", "kubectl delete {{ application_name }}")])]
-                else:
-                    main_file = [UnsortableOrderedDict([
-                        ("name", "copy json template file"),
-                        ("copy", UnsortableOrderedDict({
-                            ("src", str(role) + '.json'),
-                            ("dest", "{{ json_path }}")}))
-                    ]), UnsortableOrderedDict(
-                        [("name", "Deploy " + str(role.split('-')[0]) + ' ' + str(role.split('-')[1]) + " app"),
-                         ("command", "kubectl create -f {{ json_path }}")])]
-
-                dstfile = open(pb_path + 'roles/' + role + '/tasks/main.yaml', 'w')
-                UnsortableOrderedDict.dump(main_file, dstfile)
-                dstfile.close()
-            ############################################
-            # Templates
-            for role in nc_roles:
-                if '-create' not in role and '-destroy' not in role:
-                    data = self.sf_kubernetes(sf_data, role)
-                    dstfile = open(os.path.join(pb_path, 'roles', role, 'files', role + '.json'), 'w')
-                    json.dump(data, dstfile)
-                    dstfile.close()
         except Exception as e:
             print "Exception in generate_playbook"
             log.exception(e)
 
-    @staticmethod
-    def create_roles(sf_data, name):
-        roles = [name + '-create', name + '-destroy']
-        for vnfd in sf_data['vnfd']:
-            vnf = sf_data['vnfd'][vnfd]
-            # print vnf['vdu']
-            vdu_list = vnf['vdu']
-            for vdu in vdu_list:
-                if 'vduNestedDesc' in vdu and vdu['vduNestedDesc'] is not None:
-                    vdu_nested_desc_id = vdu['vduNestedDesc']
-                    vdu_nested = SuperfluidityParser().get_nested_vdu_from_id(vdu_nested_desc_id, vnf)
-                    if vdu_nested and vdu_nested['vduNestedDescriptorType'] == 'kubernetes':
-                        roles.append(vnfd + '-' + vdu['vduId'])
-        return roles
+    def get_commands_k8s_pod_property(self, property, pod_name):
+        if property == 'podIP':
+            return {'name': "Getting {0} from {1}".format(property, pod_name),
+                    'command': str("kubectl get pods -o jsonpath='{.items[?(@.metadata.labels.name==\""+pod_name+"\")].status.podIP}' --namespace={{ application_name }}"),
+                    'register': str(pod_name+ '_ip')
+                    }
+
+    def init_dir(self, dir_path):
+        log.info("init_dir " + dir_path)
+        shutil.rmtree(dir_path, ignore_errors=True)
+        self.makedir_p(dir_path)
+
+    def init_group_vars_file(self, group_name, vars_data):
+        log.info('Init a variables file for "%s" group', group_name)
+        group_var_path = os.path.join(self.BASE, 'group_vars', group_name)
+        yaml.dump(vars_data, open(group_var_path, 'w'), default_flow_style=False, explicit_start=True,
+                  width=float("inf"))
+
+    def add_role_task(self, role_name, task_name, task_data):
+        log.info('Add %s task to %s role', task_name, role_name)
+        tasks_path = os.path.join(self.BASE, 'roles', role_name, 'tasks')
+        task_path = os.path.join(tasks_path, task_name + '.yaml')
+        self.makedir_p(tasks_path)
+        yaml.dump(task_data, open(task_path, 'w'), default_flow_style=False, explicit_start=True,
+                  width=float("inf"))
+
+    def add_role_var(self, role_name, vars_name, var_data):
+        log.info('Add %s vars to %s role', vars_name, role_name)
+        vars_path = os.path.join(self.BASE, 'roles', role_name, 'vars')
+        var_path = os.path.join(vars_path, vars_name + '.yaml')
+        self.makedir_p(vars_path)
+        yaml.dump(var_data, open(var_path, 'w'), default_flow_style=False, explicit_start=True,
+                  width=float("inf"))
+
+    def add_role_template(self, role_name, template_name, template_data):
+        log.info('Add %s template to %s role', template_name, role_name)
+        templates_path = os.path.join(self.BASE, 'roles', role_name, 'templates')
+        template_path = os.path.join(templates_path, template_name + '.j2')
+        self.makedir_p(templates_path)
+        yaml.safe_dump(yaml.load(json.dumps(template_data)), open(template_path, 'w'), default_flow_style=False,
+                       width=float("inf"))
+
+    def add_playbook_file(self, name, playbook_data):
+        log.info('Add %s playbook file', name)
+        playbook_path = os.path.join(self.BASE, name + '.yaml')
+        yaml.dump(playbook_data, open(playbook_path, 'w'), default_flow_style=False, explicit_start=True,
+                  width=float("inf"))
 
     @staticmethod
-    def sf_kubernetes(sf_data, role):
-        split_role = role.split('-')
-        # print split_role
-        vnfd = split_role[0]
-        vdu_name = split_role[1]
-        vnf = sf_data['vnfd'][vnfd]
-        vdu_list = vnf['vdu']
-        for vdu in vdu_list:
-            if vdu['vduId'] == vdu_name:
-                if 'vduNestedDesc' in vdu and vdu['vduNestedDesc'] is not None:
-                    vdu_nested_desc_id = vdu['vduNestedDesc']
-                    vdu_nested = SuperfluidityParser().get_nested_vdu_from_id(vdu_nested_desc_id, vnf)
-                    k8s_name = str(vdu_nested['vduNestedDescriptor'])
-                    data = sf_data['k8s'][k8s_name]
-                    return data
-        return None
-
-    @staticmethod
-    def get_k8s_name(sf_data, roles):
-        k8s_name = {}
-        for role in roles:
-            if "-create" not in role and "-destroy" not in role:
-                vnfd = role.split('-')[0]
-                vdu_id = role.split('-')[1]
-                vnf = sf_data["vnfd"][vnfd]
-                vdu_list = vnf['vdu']
-                for vdu in vdu_list:
-                    if vdu['vduId'] == vdu_id:
-                        if 'vduNestedDesc' in vdu and vdu['vduNestedDesc'] is not None:
-                            vdu_nested_desc_id = vdu['vduNestedDesc']
-                            vdu_nested = SuperfluidityParser().get_nested_vdu_from_id(vdu_nested_desc_id, vnf)
-                            # k8s_name.push(role)
-                            k8s_name[role] = vdu_nested['vduNestedDescriptor']
-        return k8s_name
+    def _get_properties_from_metadata(element_id, meta_name, vnf_data):
+        metadata_list = vnf_data['modifiableAttributes']['metadata']
+        for metadata in metadata_list:
+            if meta_name in metadata:
+                for prop in metadata[meta_name]:
+                    if element_id in prop:
+                        if isinstance(prop[element_id], list):
+                            meta_prop = dict(pair for d in prop[element_id] for pair in d.items())
+                            return meta_prop
+                        else:
+                            return prop[element_id]
+        return {}
 
     @staticmethod
     def makedir_p(directory):
@@ -195,3 +161,13 @@ class AnsibleUtility(object):
         """
         if not os.path.isdir(directory):
             os.makedirs(directory)
+
+
+
+
+# if __name__ == '__main__':
+#     a_test = AnsibleUtility()
+#     input_data = _load_data_from_file(open('nsd_test.json', 'r', 1))
+#     playbook_output_dir = 'nsd_test'
+#     # print input_data
+#     a_test.generate_playbook(input_data, 'nsdtest', playbook_output_dir)
